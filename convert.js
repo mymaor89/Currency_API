e8485b28688d1084029667cc0b00376b3d0bc8ab
Maor <mymaor89@gmail.com>
2020-02-19 21:55:46 +0200

    eslint autofix to all files

--- convert.js
+++ convert.js
@@ -1,9 +1,8 @@
-const currency = require('./currency')
+const currency = require('./currency');
 
-const convert = function (from,to,amount){
-    //mock
-    const rate = 3.5;
-    let result = amount / rate
-    return result;
-}
+const convert = function (from, to, amount) {
+  const rate = currency(from, to);
+  const result = amount / rate;
+  return result;
+};
 module.exports = convert;
--- currency.js
+++ currency.js
@@ -1,18 +1,17 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-    const url = "https://api.exchangerate-api.com/v4/latest/";
-    const getData = async (code) => {
-      try {
-        const response = await fetch(url+code);
-        //let result = await response.json();
-        result= await response.json()
-        rates = result.rates
-        //console.log(Object.keys(rates).length)
-        return result;
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getData("USD")
-    
-    module.exports = getData;
+const url = 'https://api.exchangerate-api.com/v4/latest/';
+const getData = async (code) => {
+  try {
+    const response = await fetch(url + code);
+    // let result = await response.json();
+    const result = await response.json();
+    // console.log(Object.keys(rates).length)
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+getData('USD');
+
+module.exports = getData;
